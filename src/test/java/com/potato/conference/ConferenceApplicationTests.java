5a80ee327b72f84390041015ca873d38324ef69f
devjin29 <h2844j@naver.com>
2025-11-10 15:15:02 +0900

    initial commit

---
+++ src/test/java/com/potato/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potato.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
